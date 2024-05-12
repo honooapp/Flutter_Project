577d716f4cd4b87740e3cbaf7b4d9165939298d0
bheow <daniele.tomasicchio.12@gmail.com>
2024-05-12 13:25:09 +0200

    update text

--- lib/Utility/Utility.dart
+++ lib/Utility/Utility.dart
@@ -17,7 +17,7 @@ class Utility {
   final String text1_third = """\n\ne\nviaggi sull'Isola delle Storie.\n""";
   final String text1_fourth = """\n\nNon ci sono profili utenti\nin honoo.\nE non ci sono i "mi piace".\nMa se trovi un honoo\nche ti piace,\npuoi salvarlo nel tuo cuore\ne,\nse ne hai voglia,\npuoi anche fare\nqualcosa in più.\n\nO molto di più.\n\nSe è come penso,\nl'ultima volta\nche ci siamo visti\n\n""";
   final String text1_fifth = """\n\navevi gli occhi chiusi\ne hai fatto la scelta giusta.\n\nMi chiamo\nVenceslao Cembalo\ne se telefoni al\n3274920811,\npuoi sentire\nun’altra volta\nla mia voce\ne continuare il viaggio.\n\n Se le cose\nstanno\ndiversamente,\nclicca sull’icona della\nhome\nin basso a sinistra.\n\nIn alto\nal centro\ndi ogni schermata\nc'è il logo\n\n""";
-  final String text1_six = """\n\nse ci clicchi sopra,\ntorni qui\na questa pagina.\n""";
+  final String text1_six = """\n\nse ci clicchi sopra,\ntorni qui\na questa pagina.\n\nTi consiglio\ndi visualizzare il sito\nda cellulare,\nperché\nnon è stata ancora\nottimizzata\nla visualizzazione\nda monitor di computer.\n""";
   final String textHome1 = 'Ti regaliamo la Luna.\nPer sempre.';
   final String textHome2 = 'Niente è per sempre,\ne nessuno può regalarti la Luna.\n\nÈ vero,\nma non per i poeti.\n\nVuoi essere un poeta di honoo?\n\nClicca sulla Bottiglia\ne componi il tuo honoo.\n\nOppure clicca sulla Luna\ne guarda gli honoo degli altri.\n\nO clicca sull’Isola\ne inizia il viaggio\nverso le tue storie.';
   final String chestHeader = 'Il tuo Cuore custodisce';
