92ff3d88f136208bd101cd596ea611aa2c62fde5
mariaandreealavric <mariaandreealavric@users.noreply.github.com>
2026-02-03 16:52:36 +0000

    Deploy web build for e3d88e8e065b355f8f2032a664b8acc5e82aee87 e3d88e8e065b355f8f2032a664b8acc5e82aee87

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,8 +8,8 @@ const RESOURCES = {"flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "icons/honoo_icon-512.png": "9ffa8db87af77fe122309e4aeed54971",
 "icons/honoo_icon-48.png": "ef17724e0caeefe32b66eb71b36265a5",
 "manifest.json": "68268adfd9ea5553746313cc93c5f887",
-"index.html": "d204123b5faede0a2fd79b26e2869876",
-"/": "d204123b5faede0a2fd79b26e2869876",
+"index.html": "43424255c7cd16130d8e95f49fe23064",
+"/": "43424255c7cd16130d8e95f49fe23064",
 "assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
 "assets/assets/icons/home_onTertiary.svg": "790e0648b9a8ba278dca0ac4cbe4bcf9",
 "assets/assets/icons/performance.png": "9145612bc2188110672faae48de648a9",
@@ -252,7 +252,7 @@ const RESOURCES = {"flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "canvaskit/skwasm.wasm": "6711032e17bf49924b2b001cef0d3ea3",
 "favicon.ico": "ed54c59b64878d2d06ec2bf2c1a3aa7d",
 "version.json": "36ebf088e792919511c898d53ed38677",
-"main.dart.js": "faf9137eb450d19936e81b383c625c59"};
+"main.dart.js": "cf745e59c29c1a0b5290c7505ca4782d"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
