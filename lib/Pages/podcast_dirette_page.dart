9473bc491886c88b2578d8d9c3b677d47113cd3c
andreea <mariandreealavric@gmail.com>
2026-03-07 16:33:12 +0100

    Fix: rimuove import inutilizzato in PodcastDirettePage per pulire flutter analyze

--- lib/Pages/podcast_dirette_page.dart
+++ lib/Pages/podcast_dirette_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:honoo/Utility/honoo_colors.dart';
-import 'package:honoo/Utility/utility.dart';
 import 'package:honoo/Widgets/honoo_standard_page.dart';
 import 'coming_soon_page.dart';
 
