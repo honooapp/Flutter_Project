8d2953de69bc65278af797cb86a8f47bf7b779b6
andreea <mariandreealavric@gmail.com>
2026-03-07 16:38:14 +0100

    LibriPage: rende i titoli cliccabili (link a ComingSoonPage) mantenendo stile; intro derivata da Utility.libriText

--- lib/Pages/libri_page.dart
+++ lib/Pages/libri_page.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:honoo/Utility/honoo_colors.dart';
 import 'package:honoo/Utility/utility.dart';
 import 'package:honoo/Widgets/honoo_standard_page.dart';
+import 'coming_soon_page.dart';
 
 class LibriPage extends StatelessWidget {
   const LibriPage({super.key});
@@ -16,14 +18,138 @@ class LibriPage extends StatelessWidget {
       height: 1.3,
     );
 
+    final String full = Utility().libriText;
+    final String firstTitle = 'Isola delle Storie';
+    final int idx = full.indexOf(firstTitle);
+    final String intro = idx >= 0 ? full.substring(0, idx) : full;
+
+    String _header(String text) => text.replaceAll('\n', ' ').trim();
+
+    final spans = <InlineSpan>[
+      TextSpan(text: intro, style: style),
+      TextSpan(
+        text: 'Isola delle Storie\n\n',
+        style: style.copyWith(
+          decoration: TextDecoration.underline,
+          fontWeight: FontWeight.w700,
+        ),
+        recognizer: TapGestureRecognizer()
+          ..onTap = () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => ComingSoonPage(
+                  header: _header('Isola delle Storie'),
+                  quote: '',
+                  bibliography: '',
+                ),
+              ),
+            );
+          },
+      ),
+      TextSpan(
+        text: 'Isole delle Storie\n(con illustrazioni di Joel Folda)\n\n',
+        style: style.copyWith(
+          decoration: TextDecoration.underline,
+          fontWeight: FontWeight.w700,
+        ),
+        recognizer: TapGestureRecognizer()
+          ..onTap = () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => ComingSoonPage(
+                  header: _header(
+                      'Isole delle Storie\n(con illustrazioni di Joel Folda)'),
+                  quote: '',
+                  bibliography: '',
+                ),
+              ),
+            );
+          },
+      ),
+      TextSpan(
+        text: 'Immacolato\n\n',
+        style: style.copyWith(
+          decoration: TextDecoration.underline,
+          fontWeight: FontWeight.w700,
+        ),
+        recognizer: TapGestureRecognizer()
+          ..onTap = () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => ComingSoonPage(
+                  header: _header('Immacolato'),
+                  quote: '',
+                  bibliography: '',
+                ),
+              ),
+            );
+          },
+      ),
+      TextSpan(
+        text: 'I limoni sono finiti\n\n',
+        style: style.copyWith(
+          decoration: TextDecoration.underline,
+          fontWeight: FontWeight.w700,
+        ),
+        recognizer: TapGestureRecognizer()
+          ..onTap = () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => ComingSoonPage(
+                  header: _header('I limoni sono finiti'),
+                  quote: '',
+                  bibliography: '',
+                ),
+              ),
+            );
+          },
+      ),
+      TextSpan(
+        text: 'Almeno\navresti potuto\ncambiare i nomi\n\n',
+        style: style.copyWith(
+          decoration: TextDecoration.underline,
+          fontWeight: FontWeight.w700,
+        ),
+        recognizer: TapGestureRecognizer()
+          ..onTap = () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => ComingSoonPage(
+                  header: _header('Almeno\navresti potuto\ncambiare i nomi'),
+                  quote: '',
+                  bibliography: '',
+                ),
+              ),
+            );
+          },
+      ),
+      TextSpan(
+        text: 'Papà,\nma tu mi vuoi bene?\n',
+        style: style.copyWith(
+          decoration: TextDecoration.underline,
+          fontWeight: FontWeight.w700,
+        ),
+        recognizer: TapGestureRecognizer()
+          ..onTap = () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => ComingSoonPage(
+                  header: _header('Papà,\nma tu mi vuoi bene?'),
+                  quote: '',
+                  bibliography: '',
+                ),
+              ),
+            );
+          },
+      ),
+    ];
+
     return HonooStandardPage(
       contentWidthFactor: 0.45,
-      child: Text(
-        Utility().libriText,
-        style: style,
+      child: RichText(
         textAlign: TextAlign.center,
+        text: TextSpan(children: spans),
       ),
     );
   }
 }
-
