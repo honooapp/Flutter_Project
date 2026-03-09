dc1950bbc8b6e41b405a8e0f1e31d96617d6c9e9
andreea <mariandreealavric@gmail.com>
2026-03-09 18:20:58 +0100

    ui: prevent swipe on download tap (capture vertical/horizontal drag in TextBoxDownloadButton)

--- lib/Widgets/text_box_download_button.dart
+++ lib/Widgets/text_box_download_button.dart
@@ -14,24 +14,31 @@ class TextBoxDownloadButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Intercetta i drag per evitare che PageView/ListView genitori
+    // interpretino il gesto come swipe; consente comunque il tap.
     return Tooltip(
       message: tooltip,
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          onTap: onPressed,
-          borderRadius: BorderRadius.circular(size / 2),
-          child: Container(
-            width: size,
-            height: size,
-            decoration: BoxDecoration(
-              color: Colors.black.withOpacity(0.55),
-              borderRadius: BorderRadius.circular(size / 2),
-            ),
-            child: Icon(
-              Icons.download_outlined,
-              size: size * 0.6,
-              color: Colors.white,
+      child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onVerticalDragStart: (_) {},
+        onHorizontalDragStart: (_) {},
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: onPressed,
+            borderRadius: BorderRadius.circular(size / 2),
+            child: Container(
+              width: size,
+              height: size,
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.55),
+                borderRadius: BorderRadius.circular(size / 2),
+              ),
+              child: Icon(
+                Icons.download_outlined,
+                size: size * 0.6,
+                color: Colors.white,
+              ),
             ),
           ),
         ),
