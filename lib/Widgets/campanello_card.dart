455f33ffa6766034a0884b54456fc5c6352bd7ae
andreea <mariandreealavric@gmail.com>
2026-08-10 16:52:56 +0200

    Mantiene in alto il testo dei campanelli

--- lib/Widgets/campanello_card.dart
+++ lib/Widgets/campanello_card.dart
@@ -29,7 +29,7 @@ class CampanelloCard extends StatelessWidget {
       fontWeight: FontWeight.w400,
     );
 
-    final Widget text = Padding(
+    final Widget introText = Padding(
       padding: EdgeInsets.symmetric(
         horizontal: HinooTypography.horizontalPadding,
         vertical: verticalPadding,
@@ -39,9 +39,23 @@ class CampanelloCard extends StatelessWidget {
 
     if (data.isIntro) {
       return Center(
-        child: SizedBox(width: width, height: height, child: text),
+        child: SizedBox(width: width, height: height, child: introText),
       );
     }
+
+    final Widget savedText = Padding(
+      padding: EdgeInsets.fromLTRB(
+        HinooTypography.horizontalPadding,
+        HinooTypography.editorTextTopPadding(width),
+        HinooTypography.horizontalPadding,
+        HinooTypography.editorTextBottomPadding,
+      ),
+      child: Align(
+        key: const ValueKey('campanello-saved-text-position'),
+        alignment: Alignment.topCenter,
+        child: _buildText(textStyle),
+      ),
+    );
 
     return Center(
       child: ClipRRect(
@@ -52,11 +66,8 @@ class CampanelloCard extends StatelessWidget {
           child: Stack(
             fit: StackFit.expand,
             children: [
-              Image(
-                image: data.campanello!.backgroundImage,
-                fit: BoxFit.cover,
-              ),
-              text,
+              Image(image: data.campanello!.backgroundImage, fit: BoxFit.cover),
+              savedText,
             ],
           ),
         ),
